f00401e4e68315f4b1e21d06feaa7899cf0627ba
Karan Kanwar <98118264+binge-coder@users.noreply.github.com>
2023-08-27 18:30:54 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/zero_cross_project",
+  base: "/TicTacToe",
   plugins: [react()],
 })
