dd533851997c6b9b35ca72f88e59aec5023b9f66
Karan Kanwar <98118264+binge-coder@users.noreply.github.com>
2023-09-14 07:25:21 +0530

    less rounded darkmode button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ import { VscDebugRestart } from "react-icons/vsc";
 const Navbar = (props) => {
   return (
     <div className='flex flex-row justify-end px-3 py-1 mb-auto'>
-      <button onClick={props.toggleDarkMode} className='flex flex-row gap-x-2 border text-black border-black rounded-xl px-2 py-1 shadow-md bg-[#b8c1ec] dark:bg-slate-700 dark:border-white dark:text-white'>
+      <button onClick={props.toggleDarkMode} className='flex flex-row gap-x-2 border text-black border-black rounded-md px-2 py-1 shadow-md bg-[#b8c1ec] dark:bg-slate-700 dark:border-white dark:text-white'>
         <div >Dark Mode</div>
         <img className='w-6 dark:invert' src={darkModeLogo} alt="mylogo" /> 
         </button>
