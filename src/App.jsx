83123712b84dcc72acbfb66d3efd285cb65ea9ac
Karan Kanwar <98118264+binge-coder@users.noreply.github.com>
2023-12-17 03:00:34 +0530

    current- not working minmax

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,19 @@ import Navbar from './components/Navbar';
 import { VscDebugRestart } from "react-icons/vsc";
 import { IconContext } from "react-icons";
 
-function Square({ value, onSquareClick }) {
-  // const winningSquare = true;
-  // dark:bg-slate-800
-  const winningSquare = false;
-
+function Square({ value, onSquareClick, winningSquareBool }) {
   return (
-    <button className={`square border border-black text-lg font-bold h-12 w-12 p-0 text-center hover:border-4 active:bg-slate-500  dark:border-white shadow-lg ${winningSquare?'bg-green-500':'bg-[#b8c1ec] dark:bg-slate-800'}`} onClick={onSquareClick}>
+    <button className={`square border border-black text-lg font-bold h-12 w-12 p-0 text-center hover:border-4 active:bg-slate-500  dark:border-white shadow-lg ${winningSquareBool?'bg-green-500':'bg-[#b8c1ec] dark:bg-slate-800'}`} onClick={onSquareClick}>
       {value}
     </button>
   );
 }
 
 function Board({ xIsNext, squares, onPlay, handleRestart }) {
+  const [gameStatus, setGameStatus] = useState('No winner yet');
+  const [winningSquares, setWinningSquares] = useState([]);
   function handleClick(i) {
-    if (calculateWinner(squares) || squares[i]) {
+    if (calculateWinner(squares).winner || squares[i]) {
       return;
     }
     const nextSquares = squares.slice();
@@ -30,14 +28,17 @@ function Board({ xIsNext, squares, onPlay, handleRestart }) {
     onPlay(nextSquares);
   }
 
-  const winner = calculateWinner(squares);
-  let gameStatus;
-  if (winner) {
-    gameStatus = 'Winner: ' + winner + ' (computer)';
-  }
-  else {
-    gameStatus = 'No winner yet';
-  }
+  const result = calculateWinner(squares);
+  const {winner, winningCombination} = result;
+  // let gameStatus;
+  useEffect(() => {
+    if (winner) {
+      setGameStatus('Winner: ' + winner + ' (computer)');
+      setWinningSquares(winningCombination);
+    } else {
+      setGameStatus('No winner yet');
+    }
+  }, [squares]);
   let sqRange = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 
   return (
@@ -45,7 +46,12 @@ function Board({ xIsNext, squares, onPlay, handleRestart }) {
       <div className='font-bold text-center mt-2'>Play Here</div>
       <div className="grid grid-cols-3 gap-1 mx-5 my-2">
         {sqRange.map((index) => (
-          <Square key={index} value={squares[index]} onSquareClick={() => handleClick(index)} />
+          <Square 
+          key={index} 
+          value={squares[index]} 
+          onSquareClick={() => handleClick(index)}
+          winningSquareBool={winningSquares.includes(index)}
+          />
         ))}
       </div>
       <div className="mb-2 underline underline-offset-4 text-center">{gameStatus}</div>
@@ -74,13 +80,13 @@ export default function Game() {
 
   function findBestMove(squares) {
     let bestScore = -Infinity;
-    let bestMove;
-
-    for (let i = 0; i < squares.length; i++) {
-      if (squares[i] === null) {
-        squares[i] = 'O';
-        let score = minimax(squares, 0, -Infinity, Infinity, false);
-        squares[i] = null;
+    let bestMove = null;
+    let squaresCopy = [...squares];
+    for (let i = 0; i < squaresCopy.length; i++) {
+      if (squaresCopy[i] === null) {
+        squaresCopy[i] = 'O';
+        let score = minimax(squaresCopy, 0, -Infinity, Infinity, false);
+        squaresCopy[i] = null;
         if (score > bestScore) {
           bestScore = score;
           bestMove = i;
@@ -94,23 +100,25 @@ export default function Game() {
 
   function minimax(squares, depth, alpha, beta, isMaximizing) {
     const winner = calculateWinner(squares);
-    if (winner === 'X') {
+    if (winner.winner === 'X') {
       return -10 + depth;
-    }
-    if (winner === 'O') {
+    } else if (winner.winner === 'O') {
       return 10 - depth;
+    } else if (winner.winner === null && winner.winningCombination.length === 0) {
+      return 0; // Return score for draw
     }
-    if (squares.indexOf(null) === -1) {
-      return 0;
-    }
-
+    // if (squares.indexOf(null) === -1) {
+    //   return 0;
+    // }
+    const squaresCopy = squares.slice();
     if (isMaximizing) {
       let maxScore = -Infinity;
+
       for (let i = 0; i < squares.length; i++) {
-        if (squares[i] === null) {
-          squares[i] = 'O';
-          const score = minimax(squares, depth + 1, alpha, beta, false);
-          squares[i] = null;
+        if (squaresCopy[i] === null) {
+          squaresCopy[i] = 'O';
+          const score = minimax(squaresCopy, depth + 1, alpha, beta, false);
+          squaresCopy[i] = null;
           maxScore = Math.max(maxScore, score);
           alpha = Math.max(alpha, score);
           if (beta <= alpha) {
@@ -122,10 +130,10 @@ export default function Game() {
     } else {
       let minScore = Infinity;
       for (let i = 0; i < squares.length; i++) {
-        if (squares[i] === null) {
-          squares[i] = 'X';
-          const score = minimax(squares, depth + 1, alpha, beta, true);
-          squares[i] = null;
+        if (squaresCopy[i] === null) {
+          squaresCopy[i] = 'X';
+          const score = minimax(squaresCopy, depth + 1, alpha, beta, true);
+          squaresCopy[i] = null;
           minScore = Math.min(minScore, score);
           beta = Math.min(beta, score);
           if (beta <= alpha) {
@@ -212,8 +220,8 @@ function calculateWinner(squares) {
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+      return {winner: squares[a], winningCombination: [a, b, c]};
     }
   }
-  return null;
+  return {winner: null, winningCombination: []};
 }
